d10078b307935edd0a76ffdcd88c906a5249541f
Michael Wu <mw10013@gmail.com>
2022-02-11 14:41:42 -0500

    heartbeat

--- src/commands/heartbeat.ts
+++ src/commands/heartbeat.ts
@@ -1,25 +1,53 @@
-import { Command } from "@oclif/core";
+import { Command, Flags } from "@oclif/core";
 // const fetch = require("node-fetch");
 import fetch from "node-fetch";
 
 export default class Cmd extends Command {
   static description = "Post heartbeat to access cloud.";
   static examples = ["<%= config.bin %> <%= command.id %>"];
+  static enableJsonFlag = true;
 
-  static flags = {};
-
+  static flags = {
+    host: Flags.string({
+      char: "h",
+      description: "Heartbeat api host",
+      default: "http://localhost:3000",
+    }),
+  };
   static args = [];
 
-  async run(): Promise<void> {
-    this.log("heartbeat");
-    const body = { a: 1 };
-
-    fetch("https://httpbin.org/post", {
+  async run(): Promise<any> {
+    const { flags } = await this.parse(Cmd);
+    const body = {
+      accessManager: {
+        id: 1,
+      },
+    };
+    const response = await fetch(`${flags.host}/api/accessmanager/heartbeat`, {
       method: "post",
       body: JSON.stringify(body),
       headers: { "Content-Type": "application/json" },
-    })
-      .then((res) => res.json())
-      .then((json) => console.log(json));
+    });
+    if (!response.ok) {
+      const text = await response.text();
+      this.error(text);
+      return { error: text };
+    }
+
+    if (response.ok) {
+      const json = await response.json();
+      this.log(json);
+      return json;
+    }
   }
 }
+
+//   const body = { a: 1 };
+//   fetch("https://httpbin.org/post", {
+//     method: "post",
+//     body: JSON.stringify(body),
+//     headers: { "Content-Type": "application/json" },
+//   })
+//     .then((res) => res.json())
+//     .then((json) => console.log(json));
+// }
