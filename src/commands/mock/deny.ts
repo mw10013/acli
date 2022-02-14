7e16a91c2fa37bc32cf0f147f9fabd8adc4f8630
Michael Wu <mw10013@gmail.com>
2022-02-13 22:25:48 -0500

    deny

---
+++ src/commands/mock/deny.ts
@@ -0,0 +1,51 @@
+import { Command, Flags } from "@oclif/core";
+import { PrismaClient } from "@prisma/client";
+
+export default class Cmd extends Command {
+  static description = "Mock access deny";
+  static examples = ["<%= config.bin %> <%= command.id %>"];
+  static enableJsonFlag = true;
+
+  static flags = {
+    point: Flags.integer({
+      char: "p",
+      description: "point id",
+      required: true,
+    }),
+    code: Flags.string({
+      char: "c",
+      description: "code",
+      default: "1357",
+    }),
+  };
+
+  static args = [];
+
+  async catch(error: Error): Promise<any> {
+    // base class seems to swallow error
+    throw error;
+  }
+
+  async run(): Promise<any> {
+    const { flags } = await this.parse(Cmd);
+
+    const db = new PrismaClient();
+    const accessPoint = await db.accessPoint.findUnique({
+      where: { id: Number(flags.point) },
+      rejectOnNotFound: true,
+    });
+
+    const accessEvent = await db.accessEvent.create({
+      data: {
+        at: new Date(),
+        access: "deny",
+        code: flags.code,
+        accessPointId: accessPoint.id,
+      },
+    });
+
+    await db.$disconnect();
+    this.log("Grant Event", accessEvent);
+    return { accessEvent };
+  }
+}
