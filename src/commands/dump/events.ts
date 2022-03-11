f789a4880219360b09e71fe6a67b44465a2904b4
Michael Wu <mw10013@gmail.com>
2022-03-11 14:46:01 -0500

    logging

--- src/commands/dump/events.ts
+++ src/commands/dump/events.ts
@@ -29,7 +29,26 @@ export default class Cmd extends Command {
   async run(): Promise<any> {
     const { flags } = await this.parse(Cmd);
 
-    const db = new PrismaClient();
+    // const db = new PrismaClient();
+    // const db = new PrismaClient({ log: ["query", "info", "warn", "error"] });
+    const db = new PrismaClient({
+      log: [
+        {
+          emit: "event",
+          level: "query",
+        },
+        "info",
+        "warn",
+        "error",
+      ],
+    });
+
+    db.$on("query", (e) => {
+      console.log("Query: " + e.query);
+      console.log("Params: " + e.params);
+      console.log("Duration: " + e.duration + "ms");
+    });
+
     const accessEvents = await db.accessEvent.findMany({
       take: flags.take,
       skip: flags.skip,
